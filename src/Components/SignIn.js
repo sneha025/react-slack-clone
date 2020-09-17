0f4b12b108e83515fbda93d5f9acbf28c4ea9669
sneha025 <simaurya96@gmail.com>
2020-09-17 14:35:29 +0530

    first commit

---
+++ src/Components/SignIn.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class SignIn extends Component {
+  render() {
+    return (
+      <div className="signin-form">
+        <h1>SignIn</h1>
+        <button>Sign in with Google</button>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
